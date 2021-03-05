47b79caa309e4190da932863e4251cf2853f8fce
Jason Perelshteyn <jperelshteyn@gmail.com>
2021-03-05 12:20:04 -0500

    basic http server

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # syndio_hw
-Syndio Take Home Exercise
+## About
+Local HTTP server for employee data
+
+## Requirements
+
+Linux or MacOS machine
+
+[Go](https://golang.org/doc/install)
+
+## Installation
+```bash
+go get github.com/jperelshteyn/syndio_hw
+```
+
+## Usage
+```bash
+go run $(go env GOPATH)/main.go [-port] [-seed_db] [-db_path]
+```
+
+### Optional Flags
+- -port: local port to serve HTTP requests (defaults to 8888)
+- -seed_db: cleans and fills DB with seed employee data (defaults to false)
+- -db_path: path to SQLite DB file, which gets created if does not exist (defaults to current working directory)
+
+## Access API
+```bash
+curl localhost:8888/employees
+```
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Employee struct {
+	Id     int    `json:"id"`
+	Gender string `json:"gender"`
+}
+
+func employees(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT id, gender FROM employees")
+		if err != nil {
+			fmt.Fprintf(w, "server error")
+			return
+		}
+		defer rows.Close()
+		var employees []Employee
+		for rows.Next() {
+			var e Employee
+			if err := rows.Scan(&e.Id, &e.Gender); err != nil {
+				fmt.Fprintf(w, "server error")
+				return
+			}
+			employees = append(employees, e)
+		}
+		resp, err := json.Marshal(employees)
+		if err != nil {
+			fmt.Fprintf(w, "server error")
+			return
+		}
+		w.Write(resp)
+	}
+}
+
+func initDB(dbPath string) (*sql.DB, error) {
+	if info, err := os.Stat(dbPath); os.IsNotExist(err) {
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create DB file: %s", err)
+		}
+		file.Close()
+	} else if info.IsDir() {
+		return nil, fmt.Errorf("db_path %q points to directory, instead of file", dbPath)
+	}
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open DB file: %s", err)
+	}
+	return db, nil
+}
+
+func createDbSchema(db *sql.DB) error {
+	employees := `
+        CREATE TABLE IF NOT EXISTS employees (
+            id INT PRIMARY KEY,        
+            gender TEXT
+        );`
+	if _, err := db.Exec(employees); err != nil {
+		return fmt.Errorf("failed to create employees table: %s", err)
+	}
+	return nil
+}
+
+func seedDb(db *sql.DB) error {
+	insert := `
+        DELETE FROM employees;
+        INSERT INTO employees
+            (id, gender)
+        VALUES 
+            (1, "male"),
+            (2, "male"),
+            (3, "male"),
+            (4, "female"),
+            (5, "female"),
+            (6, "female");`
+	if _, err := db.Exec(insert); err != nil {
+		return fmt.Errorf("failed to seed employees table: %s", err)
+	}
+	return nil
+}
+
+func main() {
+	var port int
+	var dbPath string
+	var seed bool
+	flag.IntVar(&port, "port", 8888, "Port number for serving HTTP requests")
+	if port < 0 || port > 65353 {
+		panic(fmt.Sprintf("invalid port number, expecting between 0 and 65353, but got %d", port))
+	}
+	flag.StringVar(&dbPath, "db_path", "./employees.db", "Path to SQLite DB file")
+	flag.BoolVar(&seed, "seed_db", false, "Seed DB with initial employee data")
+	flag.Parse()
+	db, err := initDB(dbPath)
+	if err != nil {
+		panic(fmt.Sprintf("failed to open DB file: %s", err))
+	}
+	if err := createDbSchema(db); err != nil {
+		panic(fmt.Sprintf("failed to create DB schema: %s", err))
+	}
+	if seed {
+		if err := seedDb(db); err != nil {
+			panic(fmt.Sprintf("failed to seed DB %s", err))
+		}
+	}
+	http.HandleFunc("/employees", employees(db))
+	fmt.Printf("Serving employee data from %q on localhost:%d\n", dbPath, port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
